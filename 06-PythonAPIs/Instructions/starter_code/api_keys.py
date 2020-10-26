7731d36a8891303b61fe3f56561e0ca535e812e7
JorgeVillarrealG <j.villarreal94.g@gmail.com>
2020-10-26 16:00:56 -0600

    Solved Data

--- 06-PythonAPIs/Instructions/starter_code/api_keys.py
+++ 06-PythonAPIs/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "c2321858cb3fd0bae151719410009aa5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyABu65Kc9GU-EjvKuaOxb8ihagmgk9szwc"
